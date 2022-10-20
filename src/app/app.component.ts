57b29077321d6a53a6a4261653fc0cce8f79f448
Bruno Coelho <brunocoelhonet@gmail.com>
2022-10-20 17:22:45 -0300

    ex3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { group } from 'console';
 
 @Component({
   selector: 'app-root',
@@ -8,15 +9,17 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'landing_page';
-  
-  comentario = new FormGroup({
-  nome : new FormControl(),
-  sobrenome : new FormControl(),
-  email : new FormControl(),
-  senha : new FormControl(),
-  idade : new FormControl(),
-  regiao : new FormControl(),
-  tecnologias : new FormControl()
-  });
-  
+
+  comentario: FormGroup;
+  nome: FormControl;
+  email: FormControl;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.comentario = formBuilder.group({
+      nome: formBuilder.control(""),
+      email: formBuilder.control("")
+    });
+  }
+
 }
